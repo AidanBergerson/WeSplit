155ec8ad9ec242780ac1b47c5b64c1868dd2b83b
aidanbergerson <aidan.bergerson@gmail.com>
2025-01-05 16:02:49 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Aidan Bergerson on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
